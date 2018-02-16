4a9165488e633ff72b953c075b92cb8536b76cb3
Miles Porter <milesporter@Miless-MacBook-Pro-2.local>
2018-02-16 10:49:49 -0600

    A few tweaks.

--- logistics_map_net.py
+++ logistics_map_net.py
@@ -11,7 +11,7 @@
 
 import numpy
 import pandas
-from keras.models import Model
+from keras.models import Model, Sequential
 from keras.layers import Dense, Input
 from matplotlib import pyplot
 
@@ -26,21 +26,30 @@ Y = dataset[:, 1]
 
 # Set up the network
 def baseline_model():
-    inputs = Input(shape=(1,))
-    x = Dense(30, activation='sigmoid')(inputs)
-    predictions = Dense(1, activation='sigmoid')(x)
-    model = Model(inputs=inputs, outputs=predictions)
-    model.summary()
-    model.compile(optimizer='rmsprop',
-                  loss='mean_squared_error',
-                  metrics=['accuracy'])
-    return model
+
+    the_model = Sequential()
+    the_model.add(Dense(30, activation='sigmoid', input_dim=1))
+    the_model.add(Dense(1))
+
+   # inputs = Input(shape=(1,))
+    #x = Dense(30, activation='sigmoid')(inputs)
+    #predictions = Dense(1, activation='sigmoid')(x)
+    #model = Model(inputs=inputs, outputs=predictions)
+
+    the_model.summary()
+    the_model.compile(optimizer='rmsprop',
+                  loss='mean_squared_error')
+    return the_model
 
 
 # Do the work
 model = baseline_model()
 results = model.fit(X, Y, epochs=1000, batch_size=2, verbose=1)
 print("Training is complete.\n")
+
+pyplot.subplot(2, 1, 1)
+pyplot.title("Error")
+pyplot.plot(results.history['loss'])
 
 # Make predictions
 
@@ -55,6 +64,8 @@ print("The prediction is:{0}".format(prediction))
 # Plot the predicted and expected results
 expected = newdata * 4.0 * (1.0 - newdata)
 
+pyplot.subplot(2, 1, 2)
+pyplot.title("Results")
 pyplot.scatter(newdata, prediction, s=1, color="red")
 pyplot.scatter(newdata, expected, s=1, color="blue")
 pyplot.show()
