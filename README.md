2fa721f6a7a805be4426cefea81702ec1e19491f
Miles Porter <mporter@peoplenetonline.com>
2018-09-07 12:38:17 -0500

    A little cleanup and some comments.

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # simple_neural_net
+
+This is a simple program that trains a neural network to learn to predict items in the logistics map.  More information
+on the logistics map can be found [here](https://en.wikipedia.org/wiki/Feigenbaum_constants).
+ 
+ In order to run this code, you will need a few things:
+ 
+ + Python 3
+ + Several Python Libraries (these can be installed with pip):
+    + Keras 2.1.6
+    + Numpy 1.14.3
+    + Pandas 0.23.0
+    + Tensorflow 1.6
+    + Matplotlib 2.2.2
+    
+(Other versions may work.  These are just what I am running.)
+
+To run the script, issue the following command at a command prompt:
+```python 
+python logistics_map_net.py
+```
+
+The program will train a simple dense neural network based on data in the logistics.csv file.  The network predicts
+the next value in a sequence of random numbers based on the last value in the sequence.  For a discussion of similar
+work please refer to [this paper](https://www.osti.gov/biblio/5470451-nonlinear-signal-processing-using-neural-networks-prediction-system-modelling)
+.
+
+The final output will display a graph with two panes.  The top pane shows the "learning curve" of the network.  The
+bottom pane shows how the network performs on unseen data.  The BLUE dots in the bottom pane represent expected output 
+values while the RED dots represent what the network guessed the output should be.
+
+(Lapedes, A., and Farber, R.. Nonlinear signal processing using neural networks: Prediction and system modelling. United States: N. p., 1987. Web.)
+
+Some notes: The program will train a neural network that consists of 3 layers...  one input layer of a single node, a hidden layer 
+of 30 nodes and an output layer of 1 single node.  The activation function of the network is a sigmoid function, and the
+optimizer function is [RMSProp](http://www.cs.toronto.edu/~tijmen/csc321/slides/lecture_slides_lec6.pdf).  Please refer
+to the Keras docs for more information on activation functions and optimizers available through Keras.
+
+For questions or comments, please don't hesitate to reach out to me via email.
+
+Miles Porter
+
+mporter (at) paintedharmony (dot) com
+ 
--- logistics_map_net.py
+++ logistics_map_net.py
@@ -8,7 +8,7 @@
 # Miles R. Porter, Painted Harmony Group
 # This code is free to use and distribute
 # *****************************************
-from pympler import tracker
+
 import numpy
 import pandas
 from keras.models import Model, Sequential
@@ -20,57 +20,47 @@ dataframe = pandas.read_csv("logistics.csv", delim_whitespace=True, header=None)
 dataset = dataframe.values
 
 # split into input (X) and output (Y) variables
+
 X = dataset[:, 0]
 Y = dataset[:, 1]
 
 
 # Set up the network
 
-def baseline_model():
+def neural_network_model():
 
     the_model = Sequential()
     the_model.add(Dense(30, activation='sigmoid', input_dim=1))
     the_model.add(Dense(1))
-
-   # inputs = Input(shape=(1,))
-    #x = Dense(30, activation='sigmoid')(inputs)
-    #predictions = Dense(1, activation='sigmoid')(x)
-    #model = Model(inputs=inputs, outputs=predictions)
-
     the_model.summary()
     the_model.compile(optimizer='rmsprop',
                   loss='mean_squared_error')
     return the_model
 
 
-# Do the work
-
-tr = tracker.SummaryTracker()
-
-
-model = baseline_model()
+model = neural_network_model()
 results = model.fit(X, Y, epochs=1000, batch_size=2, verbose=0)
 print("Training is complete.\n")
 
-#pyplot.subplot(2, 1, 1)
-#pyplot.title("Error")
-#pyplot.plot(results.history['loss'])
+pyplot.subplot(2, 1, 1)
+pyplot.title("Error")
+pyplot.plot(results.history['loss'])
 
 # Make predictions
 
-#newdata = numpy.array([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
-newdata = numpy.random.rand(1,100)[0]
+inputs = numpy.random.rand(1,100)[0]
 
-prediction = model.predict(newdata, batch_size=1, verbose=0)
-#print("The prediction is:{0}".format(prediction))
-
-tr.print_diff()
+prediction = model.predict(inputs, batch_size=1, verbose=0)
 
 # Plot the predicted and expected results
-expected = newdata * 4.0 * (1.0 - newdata)
+expected = inputs * 4.0 * (1.0 - inputs)
 
-#pyplot.subplot(2, 1, 2)
-#pyplot.title("Results")
-#pyplot.scatter(newdata, prediction, s=1, color="red")
-#pyplot.scatter(newdata, expected, s=1, color="blue")
-#pyplot.show()
+pyplot.subplot(2, 1, 2)
+pyplot.title("Results")
+for i in range(0, len(inputs)):
+    p = prediction[i]
+    e = expected[i]
+    pyplot.scatter(i, p, s=1, color="red")
+    pyplot.scatter(i, e, s=1, color="blue")
+
+pyplot.show()
